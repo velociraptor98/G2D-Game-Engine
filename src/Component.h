ee26f879865bf4461b6565afd55ef0ef9d03df92
Kunal Singh <kunal1998doon@gmail.com>
2020-04-20 23:43:37 +0530

    Started with the basic entity-component system

---
+++ src/Component.h
@@ -0,0 +1,13 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+class Entity;
+class Component
+{
+    public :
+    Entity* owner;
+    virtual ~Component();
+    virtual void init();
+    virtual void Update(float deltaTime);
+    virtual void Render();
+}
+#endif
